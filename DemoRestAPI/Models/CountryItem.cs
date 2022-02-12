f035b899e6b8ff38d3b0099b563bf7d493833b9f
Shoukat Malik <shaukatmalikse@gmail.com>
2022-02-12 15:30:34 +0500

    Add project files.

---
+++ DemoRestAPI/Models/CountryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoRestAPI.Models
+{
+    public class CountryItem
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        public string CountryDescription { get; set; }
+    }
+}
